b5e8e3c22b824635c0234fb7bcdf6b072bafae63
Dominik Fitz <dominikfitz@gmail.com>
2025-09-05 17:41:05 +0200

    config-server

---
+++ config-server/src/test/java/eu/irrationalcharm/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.irrationalcharm.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
