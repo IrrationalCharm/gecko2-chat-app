14c3dc71259855bc448372e88ad41a16992efc38
Dominik Fitz <dominikfitz@gmail.com>
2025-09-22 21:08:19 +0200

    Added main

---
+++ user-service/src/main/java/eu/irrationalcharm/userservice/UserServiceApplication.java
@@ -0,0 +1,22 @@
+package eu.irrationalcharm.userservice;
+
+import lombok.AllArgsConstructor;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@SpringBootApplication
+@EnableWebSecurity
+@AllArgsConstructor
+@EnableJpaRepositories
+@EnableJpaAuditing
+public class UserServiceApplication  {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceApplication.class, args);
+    }
+
+
+}
